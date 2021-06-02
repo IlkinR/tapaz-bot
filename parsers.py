a706d7b8d9602bfd4d1b5f3584725fc97245f6d5
Ilkin Ramazanov <babalke44@gmail.com>
2021-06-02 15:59:10 +0400

    Add method in ProductParser class to grab all data of the product

--- parsers.py
+++ parsers.py
@@ -1,12 +1,12 @@
+from collections import namedtuple
 from typing import Dict, Tuple
 
 from utils import get_soup
 
+Location = namedtuple('Location', ['url', 'name'])
+
 
 class ProductParser:
-    # TODO: Add doc strings to methods and class
-    # TODO: Add defensive programming into methods
-
     def __init__(self, advert_url: str) -> None:
         self.advert_url = advert_url
         self.soup = get_soup(self.advert_url)
@@ -18,7 +18,8 @@ class ProductParser:
 
     def _get_price(self) -> int:
         price_tag = self.soup.find('span', class_='price-val')
-        return int(price_tag.text)
+        price = price_tag.text.replace(' ', '')
+        return int(price)
 
     def _get_phone(self) -> str:
         phone_tag = self.soup.find('a', class_='shop-phones--number')
@@ -27,7 +28,7 @@ class ProductParser:
     def _get_location(self) -> Tuple[str, str]:
         location_tag = self.soup.find('a', class_='shop--location')
         url, name = location_tag.get('href'), location_tag.text
-        return url, name
+        return Location(url=url, name=name)
 
     def _get_data(self) -> Dict[str, str]:
         data_tags = self.soup.select('div.lot-info > p')
@@ -35,3 +36,18 @@ class ProductParser:
         data_names = [tag[0] for tag in data_tags]
         data_values = [tag[1] for tag in data_tags]
         return dict(zip(data_names, data_values))
+
+    def parse(self) -> Dict[str, str]:
+        advert_data = {}
+
+        advert_data.update(self._get_properties())
+        advert_data.update(self._get_data())
+
+        location = self._get_location()
+        advert_data['location_url'] = location.url
+        advert_data['location_name'] = location.name
+
+        advert_data['price'] = self._get_price()
+        advert_data['phone'] = self._get_phone()
+
+        return advert_data
